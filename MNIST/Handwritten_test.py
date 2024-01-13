748b315ba01c6ce329b0f6e4ce70ba2bc3c386b1
Hrushikesh Deshpande <120764431+Hrushi0d@users.noreply.github.com>
2024-01-14 01:15:41 +0530

    Update Handwritten_test.py

--- MNIST/Handwritten_test.py
+++ MNIST/Handwritten_test.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
+from sklearn.metrics import precision_score
 
 mnist = tf.keras.datasets.mnist
 (x_train,y_train),(x_test,y_test) = mnist.load_data()
@@ -13,5 +14,13 @@ x_test = tf.keras.utils.normalize(x_test, axis = 1)
 model = tf.keras.models.load_model('handwritten.model')
 loss, accuracy = model.evaluate(x_test, y_test)
 
-print(loss)
-print(accuracy)
+predictions = model.predict(x_train)
+predicted_labels = np.argmax(predictions, axis=1)
+true_labels = y_train
+
+# Calculate precision
+precision = precision_score(true_labels, predicted_labels, average='weighted')
+
+print(f'Precision: {precision}')
+print(f'Loss: {loss}')
+print(f'Accuracy: {accuracy}')
